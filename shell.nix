1982fe02636f7b22c1d6befa7e7ab3cc41b9742b
zowoq <59103226+zowoq@users.noreply.github.com>
2022-08-29 10:19:57 +1000

    update nix-shell for flakes
    copies the changes from 2b4b2e31d1b180d924a88100ff8967eced932903

--- shell.nix
+++ shell.nix
@@ -1,5 +1,9 @@
 { system ? builtins.currentSystem
-, nixpkgs ? import ./nix { inherit system; }
+, nixpkgs ? import (import ./flake.lock.nix).nixpkgs {
+    inherit system;
+    configuration = { };
+    overlays = [ ];
+  }
 }:
 nixpkgs.mkShell {
   nativeBuildInputs = [ nixpkgs.ruby ];
